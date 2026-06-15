e2ba14c8e664505e45917df251880a6c71346a6c
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-06-14 20:03:59 -0400

    done

---
+++ src/main/java/io/zipcoder/persistenceapp/DepartmentController.java
@@ -0,0 +1,22 @@
+package io.zipcoder.persistenceapp;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/API/department")
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @PostMapping
+    public Department createDepartment(@RequestBody Department department) {
+        return departmentService.createDepartment(department);
+    }
+}
